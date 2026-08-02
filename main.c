feae70e63573f3b887d291f9ec5e90b9218dc348
gabedesilva <gabriel_desilva@dlsu.edu.ph>
2026-08-02 13:49:34 +0800

    Completed main.c, edited docx, added input text files

--- main.c
+++ main.c
@@ -24,53 +24,84 @@
 
 int main()
 {
+    FILE *fp;
     graphType graph;
     list result;
+    str30 input;
 
-    graph.nVertices = 6;
-
-    // Deliberately NOT alphabetically ordered
-    strcpy(graph.vertexNames[0], "B");
-    strcpy(graph.vertexNames[1], "F");
-    strcpy(graph.vertexNames[2], "A");
-    strcpy(graph.vertexNames[3], "E");
-    strcpy(graph.vertexNames[4], "D");
-    strcpy(graph.vertexNames[5], "C");
-
-    // Initialize matrix to 0
-    for (int i = 0; i < graph.nVertices; i++)
+    printf("Input filename: ");
+    scanf("%s", input);
+    if (fp = fopen(input, "r")) // opens and reads the file with the matching name inputted
     {
-        for (int j = 0; j < graph.nVertices; j++)
+        fclose(fp);
+        kuninInput(input, &graph); // verifies the input
+
+        str30 nameOnly, out1, out2, out3, out4, out5, out6, startVertex;
+        int i;
+        int dotFound = 0; //if '.' is found
+        int vertFound = 0;
+
+        strcpy(nameOnly, input);
+
+        for (i = 0; nameOnly[i] != '\0' && dotFound == 0; i++) // loops through the string until it sees a '.'
+        {   // this loop removes the file extension
+            if (nameOnly[i] == '.')
+            {
+                nameOnly[i] = '\0'; // replaces the '.' with a null byte to end the string there
+                dotFound = 1;
+            }
+        }
+        strcpy(out1, nameOnly); // takes the file name e.g. "G"
+        strcat(out1, "-SET.TXT"); // adds the file extension to the file name e.g. "G-SET.TXT"
+
+        strcpy(out2, nameOnly); // takes the file name e.g. "G"
+        strcat(out2, "-DEGREE.TXT"); // adds the file extension to the file name e.g. "G-DEGREE.TXT"
+
+        strcpy(out3, nameOnly); // takes the file name e.g. "G" 
+        strcat(out3, "-LIST.TXT"); // adds the file extension to the file name e.g. "G-LIST.TXT"
+
+        strcpy(out4, nameOnly); // takes the file name e.g. "G"
+        strcat(out4, "-MATRIX.TXT"); // adds the file extension to the file name e.g. "G-MATRIX.TXT"
+
+        // calls the output functions to produce a text file
+        unangOutput(input, out1, &graph); 
+        ikalawangOutput(out2, &graph);
+        ikatlongOutput(out3, &graph);
+        ikaapatNaOutput(out4, &graph);
+
+        printf("Input start vertex for the traversal: ");
+        scanf("%s", startVertex);
+
+        for (i = 0; i < graph.nVertices; i++) // loops through all the vertices
         {
-            graph.matrix[i][j] = 0;
+            if (strcmp(startVertex, graph.vertexNames[i]) == 0) // checks if a specified vertix is found
+            {
+                vertFound = 1;
+            }
+            
+        }
+        if (vertFound == 0) // NOT found
+        {
+            printf("Vertex %s not found.\n", startVertex); // error message
+            return 0;
+        }
+        if (vertFound == 1) // vert is valid and is found
+        {
+            strcpy(out5, nameOnly);  // takes the file name e.g. "G"
+            strcat(out5, "-BFS.TXT"); // adds the file extension to the file name e.g. "G-BFS.TXT"
+
+            strcpy(out6, nameOnly);  // takes the file name e.g. "G"
+            strcat(out6, "-DFS.TXT"); // adds the file extension to the file name e.g. "G-DFS.TXT"
+
+            ikalimangOutput(out5, &graph, startVertex);
+            ikaanimNaOutput(out6, &graph, startVertex);
         }
     }
-
-    // Get indices
-    int A = getVertexIndex(graph, "A");
-    int B = getVertexIndex(graph, "B");
-    int C = getVertexIndex(graph, "C");
-    int D = getVertexIndex(graph, "D");
-    int E = getVertexIndex(graph, "E");
-    int F = getVertexIndex(graph, "F");
-
-    // Undirected graph
-    graph.matrix[A][B] = graph.matrix[B][A] = 1;
-    graph.matrix[A][C] = graph.matrix[C][A] = 1;
-    graph.matrix[B][D] = graph.matrix[D][B] = 1;
-    graph.matrix[B][E] = graph.matrix[E][B] = 1;
-    graph.matrix[C][F] = graph.matrix[F][C] = 1;
-
-    dfs(graph, result);
-
-    printf("DFS Result:\n");
-
-    for (int i = 0; i < graph.nVertices; i++)
+    else
     {
-        printf("%s ", result[i]);
+        printf("File %s not found.", input); // error message
+        return 0; // terminates the program
     }
-
-    printf("\n");
 
     return 0;
 }
